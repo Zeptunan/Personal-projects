1189a4578ea9f6a6acd366318d60035f72d428eb
Allan Shamseddin <allsh436@student.liu.se>
2024-12-06 21:00:16 +0100

    La till mer funktionalitet och checkar, förenklade hangman_display

--- Projekt 2 - Hangman/hangman.cpp
+++ Projekt 2 - Hangman/hangman.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <iomanip>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 using std::cout; 
@@ -22,69 +23,20 @@ string random_word_generator() {
 }
 
 void hangman_display(int hearts) {
-    if (hearts == 6) {
-        cout << "0 mistakes:\n"
-             << "  +---+\n"
-             << "  |   |\n"
-             << "      |\n"
-             << "      |\n"
-             << "      |\n"
-             << "      |\n"
-             << "========"
-             << endl;
-    }else if(hearts == 5){
-        cout << endl << "1 mistakes:\n"
-             << "  +---+\n"
-             << "  |   |\n"
-             << "  O   |\n"
-             << "      |\n"
-             << "      |\n"
-             << "      |\n"
-             << "========"
-             << endl;
-    }else if(hearts == 4){
-        cout << endl << "2 mistakes:\n"
-             << "  +---+\n"
-             << "  |   |\n"
-             << "  O   |\n"
-             << " /    |\n"
-             << "      |\n"
-             << "      |\n"
-             << "========"
-             << endl;
-    }else if(hearts == 3){
-        cout << endl << "3 mistakes:\n"
-             << "  +---+\n"
-             << "  |   |\n"
-             << "  O   |\n"
-             << " /|   |\n"
-             << "      |\n"
-             << "      |\n"
-             << "========"
-             << endl;
-    }else if(hearts == 2){
-        cout << endl << "4 mistakes:\n"
-             << "  +---+\n"
-             << "  |   |\n"
-             << "  O   |\n"
-             << " /|\\  |\n"
-             << "      |\n"
-             << "      |\n"
-             << "========"
-             << endl;    
-    }else if(hearts == 1){
-        cout << endl << "5 mistakes:\n"
-             << "  +---+\n"
-             << "  |   |\n"
-             << "  O   |\n"
-             << " /|\\  |\n"
-             << " /    |\n"
-             << "      |\n"
-             << "========"
-             << endl;           
-    }
+    
+    vector<string> hangman_stages = {
+    "\n  +---+\n  |   |\n      |\n      |\n      |\n      |\n========",
+    "1 mistake \n  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n========",
+    "2 mistakes \n  +---+\n  |   |\n  O   |\n /    |\n      |\n      |\n========",
+    "3 mistakes \n  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n========",
+    "4 mistakes \n  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n========",
+    "5 mistakes \n  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n========",
+    "6 mistakes \n  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========"
+    };  
+    
+    cout << "\n" << hangman_stages[6 - hearts] << "\n";
+    
 }
-
 
 void guess_word(){
     vector<char> wrong_guess; 
@@ -92,62 +44,68 @@ void guess_word(){
     string current_guess(actual_word.size(), '?'); 
     int hearts = 6;
     char guess_character;
-    cout << current_guess << endl; 
-    cout << "hearts: " << hearts << endl;
+    cout << "Welcome to Hangman! Try to guess the word, one letter at a time. \n" << 
+            "You have 6 attempts to save the Hangman. Good luck! \n";
+    cout << "\n" << "Currently guessed words: " << current_guess << "\n"; 
     while(current_guess != actual_word) {
         hangman_display(hearts);
-        cout << "Try to guess a character" << endl; 
+        cout << "Try to guess a character" << "\n"; 
         cin >> guess_character; 
+        guess_character = tolower(guess_character);
         bool correct_guess = false;
+
+        if(!isalpha(guess_character)){
+            cout << "Invalid input. Please enter a single letter." << "\n";
+            cin.clear();
+            cin.ignore(1000, '\n');
+            continue; 
+        }
+
+        if (find(wrong_guess.begin(), wrong_guess.end(), guess_character) != wrong_guess.end()) {
+            cout << "You already guessed that letter!" << "\n";
+            continue;
+        }
+
+
         for(int i = 0; i < actual_word.size(); i++){
             if(guess_character == actual_word[i]) {
                 correct_guess = true; 
                 current_guess[i] = guess_character;
-                cout << "hearts: " << hearts << endl;
+                cout << "hearts: " << hearts << "\n";
             }
         }
         
-        cout << "Your current guess is: " << current_guess << endl;
+        cout << "Your current guess is: " << current_guess << "\n";
         
         if(!correct_guess){
-            wrong_guess.push_back(guess_character);
-            cout << "The character: " << guess_character << " is wrong" << endl; 
+            if(isalpha(guess_character)){
+                wrong_guess.push_back(guess_character);
+            }
+            cout << "The character: " << guess_character << " is wrong" << "\n";      
+            cout << "attempts left: " << hearts << "\n";
             hearts--;
-            cout << "hearts: " << hearts << endl;
             cout << "previous guesses: "; 
             for(char c : wrong_guess){
                 cout << c << " ";
             }
         }
         
-
         if(current_guess == actual_word){
-            cout << "Congratulations, you have guessed the word!" << endl; 
+            cout << "Congratulations, you have guessed the word!" << "\n"; 
+            break;
         }
 
         if(hearts == 0){
-            cout << endl << "You have lost!" << endl  
-             << "6 mistakes:\n"
-             << "  +---+\n"
-             << "  |   |\n"
-             << "  O   |\n"
-             << " /|\\  |\n"
-             << " / \\  |\n"
-             << "      |\n"
-             << "========"             
-            << endl << "the word is: " << actual_word << endl; 
+            cout << "\n" << "You have lost!" << "\n";
+            hangman_display(0);  
+            cout << "the word is: " << actual_word << "\n"; 
             break;
         }
     }
 }
 
-void game_update(){
-  
+int main() {
     srand(static_cast<unsigned int>(time(0)));
     guess_word();
-}
-
-int main() {
-    game_update();
     return 0;
 }
